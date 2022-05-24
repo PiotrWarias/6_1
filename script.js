95517932836c46365a18baf30c732f692077981b
pwarias <93276356+PiotrWarias@users.noreply.github.com>
2022-05-24 11:43:21 +0000

    sprawdza czesciowo numer 6_2

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 (function () {
   const ex1=document.getElementById ('ex1_button')
   const ex1c=document.getElementById ('ex1_content')
-
-  ex1.addEventListener("click",function(){
+  const ex2=document.getElementById ('ex2_text')
+  const ex2c=document.getElementById ('ex2_content')
+  
+  function validTel(str){
+  str = str.replace(/[^0-9]/g, '');
+  var l = str.length;
+  if(l<9 || l>9) return ['Długość numeru musi być równa 9'];
+  var tel = '', num = str.substr(-7),
+      code = str.substr(-9, 3),
+      coCode = '';
+  if(l>9) {
+    coCode =  + str.substr(0, (l-9) );
+  }
+  tel = coCode + code + num;
+  
+  return ['okay ', tel];
+}
+  function isNumeric(num){
+  return !isNaN(num)
+}
+  
+  ex1.addEventListener("click", function(){
     var data="";
-    for(var i = 0;i < 10;i ++){
+    for(var i = 0;i < 9;i ++){
       if(i != 0)
         data += ",";
       data += i.toString();
     }
     ex1c.innerHTML=data;
+  })
+  
+  ex2.addEventListener("change", function(){
+    ex2c.innerHTML=isNumeric(ex2.value);
+    ex2c.innerHTML=validTel(ex2.value);
   })
 })();
